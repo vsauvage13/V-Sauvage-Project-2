2c78fa40b0df201b17a538d373533d1ae73d0ca1
vsauvage13 <vsauvage@northislandcollege.ca>
2025-03-27 20:53:52 -0700

    Addedlinks to google fonts and font awesome for icons

--- header.php
+++ header.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_uri()); ?>" type="text/css" />
     <?php wp_head(); ?>
     <?php the_title(); ?>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <header>
     This is the header.
