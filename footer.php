2f2f537a39f86244f17b052258f7253bea172ecc
vsauvage13 <vsauvage@northislandcollege.ca>
2025-03-27 17:28:25 -0700

    Added header and footer
    updated with instructions text.

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<footer>This is a footer!</footer>
+<body>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_uri()); ?>" type="text/css" />
+    <?php wp_head(); ?>
+    <?php the_title(); ?>
+</head>
+<header>
+    This is the header.
+</header>
+
+</html>
