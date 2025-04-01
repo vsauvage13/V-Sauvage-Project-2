d73869df628aa1ab4621f3af9beb29836fd5de22
vsauvage13 <vsauvage@northislandcollege.ca>
2025-04-01 09:28:34 -0700

    Updated Header code

--- header.php
+++ header.php
@@ -5,12 +5,12 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <link rel="stylesheet" href="<?php echo esc_url(get_stylesheet_uri()); ?>" type="text/css" />
     <?php wp_head(); ?>
-    <?php the_title(); ?>
+    <!-- < ?php the_title(); ?> displays word home -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <header>
-    This is the header.
+<?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
 </header>
 
 </html>
