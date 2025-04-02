b6307ff866f1dfb3d75d4f6bc8352ea30e9e6d18
vsauvage13 <vsauvage@northislandcollege.ca>
2025-04-01 21:55:04 -0700

    Added image and css class
    added to footer

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
-<footer><?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?></footer>
+<footer>
+<img class="footer-logo" src="<?php echo get_template_directory_uri(); ?>/images/SSC-Logo-Colour-Wordmark-and-Half-Sun.png" alt="colour logo and wordmark">
+<?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?></footer>
 
 
 </html>
