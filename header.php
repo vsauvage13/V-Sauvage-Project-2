825767c11e71ae7741bdf952284a6e8dbadff66f
vsauvage13 <vsauvage@northislandcollege.ca>
2025-04-01 21:55:16 -0700

    Added logo to header - added class for css

--- header.php
+++ header.php
@@ -10,7 +10,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <header>
-<?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
+    <img class="header-logo" src="<?php echo get_template_directory_uri(); ?>/images/SSC-Logo-Colour-Wordmark-and-Half-Sun.png" alt="colour logo and wordmark">
+    <?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
 </header>
 
 </html>
